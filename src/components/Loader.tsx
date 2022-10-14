1cd98c46d1d67841aa86fa28ffe3091f35770f77
Liam Welsh <liamwelsh17@gmail.com>
2022-10-14 18:20:07 -0300

    Changes loader to not display open modals when loading

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,16 @@
 import { Grid } from "react-loader-spinner";
 import "../index.css";
 const Loader = ({ isLoading }: { isLoading: boolean }) => {
-  return isLoading ? (
-    <div className="loader">
-      <Grid color="#1982FC" height={100} width={200} />
+  return (
+    <div className="loaderContainer">
+      <Grid
+        color="#1982FC"
+        height={100}
+        width={200}
+        visible={isLoading}
+        wrapperClass="loader"
+      />
     </div>
-  ) : (
-    <></>
   );
 };
 
