46b99342d405401d2daae56b852522b371e2d80d
Liam Welsh <liamwelsh17@gmail.com>
2022-10-15 16:03:58 -0300

    Adds times to messages, fixes white screen on loader

--- src/components/messageTile.tsx
+++ src/components/messageTile.tsx
@@ -1,7 +1,8 @@
 import { Message, User } from "../models";
 import { useQuery } from "react-query";
 import useApi from "../hooks/useApi";
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import moment from "moment";
 
 interface MessageTileProps {
   messages: Message[];
@@ -26,18 +27,34 @@ export default function messageTile({
 
   return (
     <div className="messagesContent" ref={messageContentRef}>
-      {messages.map((message, i) => (
-        <div
-          key={i}
-          className={
-            message.userId === loggedInUser?.id
-              ? "sentMessage"
-              : "recivedMessage"
-          }
-        >
-          {message.message}
-        </div>
-      ))}
+      {messages.map((message, i) => {
+        const currentMessageDate = moment(message.created_at);
+        const prevMessageDate = moment(
+          messages[i - 1]?.created_at ?? message.created_at
+        );
+        const sameHour =
+          currentMessageDate.dayOfYear() === prevMessageDate.dayOfYear() &&
+          currentMessageDate.hour() - prevMessageDate.hour() === 0;
+
+        return (
+          <React.Fragment key={i}>
+            {(i === 0 || !sameHour) && (
+              <div className="messageTime">
+                {moment(message.created_at).calendar()}
+              </div>
+            )}
+            <div
+              className={
+                message.userId === loggedInUser?.id
+                  ? "sentMessage"
+                  : "recivedMessage"
+              }
+            >
+              {message.message}
+            </div>
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 }
