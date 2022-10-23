7a33f9ff2493ae15f8143ebf1549eb00074c2554
Liam Welsh <liamwelsh17@gmail.com>
2022-10-23 18:28:43 -0300

    Redirects user back to login if stored token is invalid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import Home from "./components/Home";
 import Login from "./components/Login";
-import { Navigate, Outlet, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { Navigate, Outlet, Route, Routes, useNavigate } from "react-router-dom";
+import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
 import Register from "./components/Register";
 import useAuth from "./hooks/auth/useAuth";
-import { AuthContext, AuthContextTypes } from "./hooks/auth/AuthContext";
+import { AuthContext } from "./hooks/auth/AuthContext";
+import toast from "react-hot-toast";
 
 function App() {
   const { storedUser, storedToken, setStoredToken, setStoredUser } = useAuth();
+  const navigate = useNavigate();
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         refetchOnWindowFocus: false,
       },
     },
+    queryCache: new QueryCache({
+      onError: (err: any, _) => {
+        if (err.message === "Request failed with status code 403") {
+          navigate("/");
+          toast.error("User is unauthorized. Please try again.");
+        }
+      },
+    }),
   });
 
   return (
